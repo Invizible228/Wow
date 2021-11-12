3b5a0f63768936e6dc3b5b5e10eaf57854c1db66
Тимур <@DESKTOP-5F4LMOP>
2021-11-12 16:37:26 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace TestEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Читаю файл...");
+            List<string[]> l = new List<string[]>();
+            StreamReader sr = new StreamReader("Vkusno.txt");
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                l.Add(line.Split(' '));
+            }
+            Console.WriteLine("Люди, вес которых кратен трем:");
+            try
+            {
+                var a = l.Where(x => Convert.ToInt32((x[3])) % 3 == 0);
+                foreach (string[] needed in a)
+                {
+                    Console.WriteLine(string.Join(" ", needed));
+                }
+            }
+            catch { Console.WriteLine("Ошибка! Неверные данные"); }
+            sr.Close();
+            Console.Write("Нажмите любую кнопку для продолжения...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tasty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestEx
+{
+    public class Tasty
+    {
+        string name = "";
+        string age = "";
+    }
+}
